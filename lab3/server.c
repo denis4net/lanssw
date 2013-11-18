13aa888f2f07c05cbc2b87c99e483b4125510769
Denis Vashchuk <denis.net@inbox.ru>
2013-11-18 23:02:06 +0300

    lab3: urgent data send\recieve implementation added

---
+++ lab3/server.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define BUFSIZE 1024
+#define RECV_STATUS_CYCLES 1024
+
+int common_readed=0;
+int sockfd;
+
+int signal_handler(int code);
+
+inline off_t file_size(int fd)
+{
+  struct stat _stat;
+  fstat(fd, &_stat);
+  return _stat.st_size;
+}
+
+int tcpv4_bind(const char* ipv4, const char* tcp_port)
+{
+   unsigned short int port = atoi( tcp_port );
+      in_addr_t ip = inet_addr( ipv4 );
+      
+      struct sockaddr_in sockaddr;   
+      memset( &sockaddr, 0x0, sizeof(sockaddr) );
+      sockaddr.sin_family = PF_INET;
+      sockaddr.sin_port = htons( port ); 
+      sockaddr.sin_addr.s_addr = ip;
+      
+      int sockfd = socket( PF_INET, SOCK_STREAM, IPPROTO_TCP );
+      int opt=1;
+      setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+      if( bind( sockfd, &sockaddr, sizeof( sockaddr ) ) )
+	return -1;
+      return sockfd;
+}
+
+int main(int argc, char** argv)
+{
+	signal(SIGURG, signal_handler);
+	signal(SIGPIPE, signal_handler);
+	
+      if( argc != 3 ) {
+	fprintf(stderr, "use: server <listen_ip> <port>\n");
+	return 1;
+      }
+    
+      sockfd = tcpv4_bind(argv[1], argv[2]);
+      if(sockfd == -1)  {
+	perror("Can't init socket");
+	return 2;
+      }
+      
+      if( listen( sockfd, 1 ) ) {
+	perror("can't start listening");
+	return 2;
+      }
+      
+      int client_sockfd;
+      printf("Start listening at %s:%s\n", argv[1], argv[2] );
+      
+      char buffer[BUFSIZE];
+      struct sockaddr_in client_addr;
+      int size = sizeof(client_addr);
+      
+      while( ( client_sockfd = accept( sockfd, &client_addr, &size ) ) != -1 ) {
+	fcntl(client_sockfd, F_SETOWN, getpid());
+	int opt=1;
+	setsockopt(client_sockfd, SOL_SOCKET, SO_KEEPALIVE, &opt, sizeof(opt)); 
+	printf("Connection opened with %s\n", inet_ntoa(client_addr.sin_addr));
+	uint32_t flen, status;
+	
+	status  = recv(client_sockfd, &flen, sizeof(flen), MSG_WAITALL);
+	if(status == -1) {
+	  perror("Can't receive file name size");
+	  return 5;
+	}
+	
+	char fname[flen+1];
+	memset(fname, 0x0, sizeof(fname));
+	status = recv(client_sockfd, fname, flen, MSG_WAITALL);
+	if(status==-1) {
+	  perror("Can't receive file name");
+	  continue;
+	}
+	
+	int fd = open(fname, O_WRONLY| O_APPEND | O_CREAT, 0755);
+	//send file open status
+	status = errno;
+	send(client_sockfd, &status, sizeof(status), 0);
+	
+	//send file offset
+	{
+	  uint32_t offset = file_size(fd);
+	  send(client_sockfd, &offset, sizeof(offset), 0x0);
+	  printf("File name: %s, file size %u bytes\n", fname, offset);	  
+	}
+	
+	//receiving data size
+	uint32_t data_size, display_status;
+	recv(client_sockfd, &data_size, sizeof(data_size), 0x0);
+	
+	while( common_readed != data_size) {
+	  status = recv( client_sockfd, buffer, sizeof(buffer), 0x0);
+	  common_readed +=  status;
+
+	  if(  status == -1 ||  status == 0 )  {
+	    perror("\nClient disconected");
+	    break;
+	  }
+	 else if( write(fd, buffer,  status) == -1 )  {
+	    perror("\nCan't write to file");
+	    break;
+	  }
+	}
+	
+	if( common_readed == data_size ) {
+	  status=0;
+	  send(client_sockfd, &status, sizeof(status), 0x0);
+	  printf("\nAll data received\n");
+	}
+	else {
+	  fprintf(stderr, "\nNot all data received from client\n");
+	}
+	close(fd);
+	close(client_sockfd);
+	printf("\nConnection closed\n\n");
+      }
+      
+      perror("Socket accept error");
+      return 0;
+}
+
+int signal_handler(int code)
+{
+	printf("signal_handler code=%d\n", code);
+	uint8_t buf;
+	switch(code) {
+		case SIGPIPE:
+			fprintf(stderr, "Pipe broken"); 
+			break;
+		case SIGURG:
+			recv(sockfd, &buf, sizeof(buf), MSG_OOB);
+			printf("Urgent data received\n");
+			printf("%10.2lf KB received\n",  common_readed/1024.0);
+			break;
+	}
+}
